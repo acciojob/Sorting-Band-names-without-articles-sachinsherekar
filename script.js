341bbcc16ddacf125d74c13210f8c6e9d22d94a8
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-06-03 13:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here
+//your code here
+function sortBands(bands) {
+  // Get a regular expression that matches any of the specified articles at the beginning of a string
+  const articleRegex = /^(a|an|the)\s/i;
 
+  // Define a comparison function that ignores articles
+  function compareBands(a, b) {
+    // Remove any articles from the beginning of the two strings
+    const aWithoutArticle = a.replace(articleRegex, '');
+    const bWithoutArticle = b.replace(articleRegex, '');
+    
+    // Compare the modified strings using the default lexicographic comparison
+    return aWithoutArticle.localeCompare(bWithoutArticle);
+  }
+  
+  // Sort the bands using the custom comparison function
+  const sortedBands = bands.sort(compareBands);
+  
+  // Generate an HTML string containing the sorted list of bands
+  const bandList = sortedBands.map(band => `<li>${band}</li>`).join('');
+  const outputHTML = `<ul id="band">${bandList}</ul>`;
+  
+  return outputHTML;
+}
+
+// const bandNames = ['The Beatles', 'Led Zeppelin', 'The Rolling Stones', 'Guns N Roses'];
+// const sortedBandsHTML = sortBands(bandNames);
+ console.log(sortedBandsHTML);
+// Output: <ul id="band"><li>Guns N Roses</li><li>The Beatles</li><li>Led Zeppelin</li><li>The Rolling Stones</li></ul>
